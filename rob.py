7e4cc0d85891c6726ac508152c20f5169540c1dd
KyanPepper <Kyankks1@gmail.com>
2025-10-23 19:52:24 -0700

    fob

--- rob.py
+++ rob.py
@@ -3,5 +3,19 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
+        length = len(nums)
+        cache = [-1] * length
+        def dfs(i):
+
+            if i >= length:
+                return 0
+            if cache[i] != -1:
+                return cache[i]
+            val = nums[i]
+            cache[i] = val + max(dfs(i+2),dfs(i+3))
+
+            return cache[i]
+
+
+        return max(dfs(0),dfs(1))
         
